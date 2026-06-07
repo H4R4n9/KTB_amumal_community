b52a0352e94aa616e0fe755a08e7f3cc270b0624
kyla.kim <kyla.kim@kylakimui-MacBookPro.local>
2026-06-08 00:30:43 +0900

    [Feat] 레포지토리 추가

---
+++ src/main/java/com/kyla/community/domain/user/ProfileImageRepository.java
@@ -0,0 +1,11 @@
+package com.kyla.community.domain.user;
+
+import com.kyla.community.domain.user.entity.ProfileImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// 프로필 이미지 저장과 회원별 최신 이미지 조회
+public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
+	Optional<ProfileImage> findFirstByUserIdOrderByProfileImageIdDesc(Long userId);
+}
